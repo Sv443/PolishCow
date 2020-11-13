105fcee2da45e3bd6a066782b3e7b8c8c5d3c57e
Sven <s.fehler@pbsnetwork.eu>
2020-11-13 04:44:34 +0100

    cow v1.1?

--- PolishCow.js
+++ PolishCow.js
@@ -2,13 +2,21 @@ const { resolve } = require("path");
 const sound = require("sound-play");
 const mm = require("music-metadata");
 const fs = require("fs");
+const asciify = require("asciify-image");
 
 const animation = require("./animation.json");
 
 const cursor = require("ansi")(process.stdout);
 
 
+/**
+ * All pre-calculated frames are saved to this object
+ * @prop {String[][][]} [0x0] Example
+ */
+var frameSets = {};
 
+
+/** Settings for Polish Cow */
 const settings = {
     /** Set to true to enable debug messages */
     dbg: false,
@@ -16,12 +24,20 @@ const settings = {
     soundFile: resolve("./polishcow.mp3"),
     /** Adds this amount of seconds to the played audio file since the sound-play package is pretty inaccurate */
     endBuffer: 0.4,
-    /** [ width, height ] - how big the terminal needs to be to play the animation */
-    minSize: [ 80, 30 ],
     /** Interval (in seconds) between the single frames of the animation */
     animationInterval: 0.2,
     /** In which sequence to play the animations */
-    animationSequence: [ 0, 1, 0, 1, 2, 3, 4, 3, 4, 2 ]
+    animationSequence: [ 0, 1, 0, 1, 2, 3, 4, 5, 4, 5, 3, 2 ],
+    /** [ Width, Height ] - By what number to decrease height or width by, globally */
+    animationPadding: [ 0, 0 ],
+    /** Options for the package asciify-image - Documentation: https://www.npmjs.com/package/asciify-image#optionscolor */
+    asciifyOpts: {
+        color: false,
+        fit: "box",
+        format: "array",
+        width: 200,
+        height: 100
+    }
 };
 
 /** @type {Boolean} Set to true to pause the animation */
@@ -37,47 +53,41 @@ var currentAnimSequenceIdx = 0;
  */
 async function PolishCow()
 {
-    if(!fs.existsSync(settings.soundFile))
+    if(!process.stdout.isTTY)
     {
-        console.log(`\n\x1b[31m\x1b[1mSound file at path "${settings.soundFile}" doesn't exist.\x1b[0m\n\n(Process exits automatically)\n`);
-        
+        console.log(`\n\x1b[31m\x1b[1mStandard output channel is not a TTY (terminal)\x1b[0m\n\n(Process exits automatically)\n`);
+
         setTimeout(() => process.exit(1), 10000);
     }
     else
     {
-        cursor.hide();
-        checkSize();
-
-        play(settings.soundFile, true);
-
-        process.stdout.on("resize", () => {
-            if(settings.dbg)
-                console.log(`RESIZE EVENT TRIGGERED`);
-
+        if(!fs.existsSync(settings.soundFile))
+        {
+            console.log(`\n\x1b[31m\x1b[1mSound file at path "${settings.soundFile}" doesn't exist.\x1b[0m\n\n(Process exits automatically)\n`);
+            
+            setTimeout(() => process.exit(1), 10000);
+        }
+        else
+        {
             cursor.hide();
-            checkSize();
-        });
 
-        startAnimation();
+            setWindowTitle("Polish Cow - Sv443");
+            play(settings.soundFile, true);
+            
+            calculateCurrentFrameSet().then(() => {
+                startAnimation();
+            });
 
-        setWindowTitle("Polish Cow - Sv443");
-    }
-}
+            process.stdout.on("resize", () => {
+                if(settings.dbg)
+                    console.log(`RESIZE EVENT TRIGGERED`);
 
-/**
- * Checks if the terminal size is okay
- */
-function checkSize()
-{
-    /** @type {Number[]} [ width, height ] */
-    let size = [ process.stdout.columns, process.stdout.rows ];
-
-    if(size[0] < settings.minSize[0] || size[1] < settings.minSize[1])
-        animationPaused = true;
-    else
-    {
-        if(animationPaused == true)
-            animationPaused = false;
+                cursor.hide();
+                calculateCurrentFrameSet().then(() => {
+                    redrawFrame();
+                });
+            });
+        }
     }
 }
 
@@ -121,11 +131,6 @@ function startAnimation()
             if(currentAnimSequenceIdx >= settings.animationSequence.length)
                 currentAnimSequenceIdx = 0;
         }
-        else
-        {
-            console.clear();
-            console.log(`\x1b[31m\x1b[1mWindow size too small!\x1b[0m\n\nExpected: ${settings.minSize[0]}x${settings.minSize[1]}\nCurrent:  ${process.stdout.columns}x${process.stdout.rows}`);
-        }
     }, settings.animationInterval * 1000);
 
     if(!animationPaused)
@@ -135,6 +140,23 @@ function startAnimation()
     }
 }
 
+/** 
+ * Calculates the frame set of the current window resolution
+ */
+function calculateCurrentFrameSet()
+{
+    return new Promise((pRes, pRej) => {
+        //TODO: parallelized frame set calculation
+    });
+}
+
+/**
+ * Redraws the current frame without incrementing any counters or advancing the animation sequence
+ */
+function redrawFrame()
+{
+    //TODO:
+}
 
 /**
  * Plays a sound file (supports MP3, WAV and other formats)
@@ -188,6 +210,29 @@ function setWindowTitle(title)
         process.stdout.write(`\x1b]2;${title}\x1b\x5c`); // *nix doesn't have a nice way to set the window title but this escape sequence should be able to do it (for reference search "OSC control sequences" on this page: https://man7.org/linux/man-pages/man4/console_codes.4.html)
     else
         process.title = title; // This should work only on Windows
+}
+
+/**
+ * Converts an image at the provided path to ASCII art with dimensions being determined by terminal size
+ * @param {String} imgSrc Path to the image
+ * @returns {Promise[String[]]} Resolves with a 2D array of strings or rejects with an error message
+ */
+function imageToResponsiveAscii(imgSrc)
+{
+    return new Promise((pRes, pRej) => {
+        imgSrc = resolve(imgSrc);
+
+        settings.asciifyOpts.width = (process.stdout.columns - settings.animationPadding[0]);
+        settings.asciifyOpts.height = (process.stdout.rows - settings.animationPadding[1]);
+
+        asciify(imgSrc, settings.asciifyOpts)
+        .then(res => {
+            return pRes(res);
+        })
+        .catch(err => {
+            return pRej(err);
+        });
+    });
 }
 
 
